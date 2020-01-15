2ec7b6d2176fb979b55b4c7023e8fc7b233bab39
Oles Marola <oles_pw@ukr.net>
2020-01-15 18:06:30 +0200

    Updates

---
+++ precache-manifest.939d94e5fc648e869f17b05915eda2f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8eb5a7cb720f3487475f8e08f5a2c5",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "d4dcb0b8edb41db61603",
+    "url": "/react_uber-eats/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "d53d5386a13a6b645c12",
+    "url": "/react_uber-eats/static/js/2.698acc84.chunk.js"
+  },
+  {
+    "revision": "d4dcb0b8edb41db61603",
+    "url": "/react_uber-eats/static/js/main.3520104d.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
